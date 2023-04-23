4dc02d86ba4ed2fdf7a0595551501481f9d7de1a
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-04-24 01:22:25 +0200

    Added user account crration functionality

--- ECommerce_DRF/User/urls.py
+++ ECommerce_DRF/User/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("<int:pk>/", UserList.as_view()),
+    path("create/", CreateUserView.as_view(), name="create_user"),
+    path("<int:pk>/edit/", EditUserView.as_view(), name="edit_user"),
 ]
--- ECommerce_DRF/User/views.py
+++ ECommerce_DRF/User/views.py
@@ -2,11 +2,33 @@ from django.shortcuts import render
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from django.contrib.auth.forms import UserCreationForm
 from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
 
 class UserList(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+
+class CreateUserView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreationForm
+    permission_classes = []
+
+class EditUserView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserCreationForm
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, pk):
+        user = get_object_or_404(User, pk=pk)
+        serializer = self.serializer_class(user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
